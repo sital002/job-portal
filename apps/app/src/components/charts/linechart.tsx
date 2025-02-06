e69c1179d3612af75574d6f767b5f03348afd6d1
bikalpa3D <vikalpradhanbca078@chitwancollege.edu.np>
2025-02-06 22:01:48 +0545

    config and knowing on line charts

--- apps/app/src/components/charts/linechart.tsx
+++ apps/app/src/components/charts/linechart.tsx
@@ -1,16 +1,12 @@
-import * as React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+import { LineChart } from "@mui/x-charts/LineChart";
 
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const uData = [3, 1, 2, 0];
+// const pData = [2400, 1398, 9800, 3908];
 const xLabels = [
-  'Page A',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
+  "Totla applied jobs",
+  "total accepted jobs",
+  "total rejected jobs",
+  "total pending jobs",
 ];
 
 export default function SimpleLineChart() {
@@ -19,10 +15,16 @@ export default function SimpleLineChart() {
       width={800}
       height={500}
       series={[
-        { data: pData, label: 'pv' },
-        { data: uData, label: 'uv' },
+        // { data: pData, label: "pv" },
+        { data: uData, label: "uv" },
       ]}
-      xAxis={[{ scaleType: 'point', data: xLabels }]}
+      xAxis={[{ scaleType: "point", data: xLabels }]}
+      yAxis={[
+        {
+          min: 0, // Set minimum value for Y-axis
+          max: 5, // Set maximum value for Y-axis // Set interval for Y-axis
+        },
+      ]}
     />
   );
 }

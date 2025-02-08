8ab2f3af3abb96309c5ced262a36026f83ddf3b8
bikalpa3D <vikalpradhanbca078@chitwancollege.edu.np>
2025-02-08 16:24:23 +0545

    minor bug fixed

--- apps/app/src/pages/bookmark-page/bookmark-page.tsx
+++ apps/app/src/pages/bookmark-page/bookmark-page.tsx
@@ -20,42 +20,44 @@ export const BookMarkPage = () => {
       {isLoading && <p>Loading...</p>}
       {error && <p>{error.message}</p>}
 
-      {bookmarks.length > 0 ? (
-        bookmarks?.map((job: Data, index: number) => (
-          <motion.div
-            key={job._id.toString()}
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-            className="bg-white rounded-lg shadow-md p-4"
-          >
-            <h3 className="text-lg font-semibold">{job.job.title}</h3>
-            <p className="text-gray-600">{job.job.company}</p>
-            <div className="flex justify-between mt-2">
-              <span className="text-sm text-gray-500">{job.job.location}</span>
-              <span className="text-sm text-gray-500">{job.job.type}</span>
-            </div>
-            <p className="text-sm text-gray-500 mt-2">
-              Posted {job.job.createdAt.slice(0, 10)}
-            </p>
-            <Link
-              to={`/jobs/${job.job._id}`}
-              className="text-blue-600 hover:underline"
+      {bookmarks && bookmarks.length > 0
+        ? bookmarks?.map((job: Data, index: number) => (
+            <motion.div
+              key={job._id.toString()}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              className="bg-white rounded-lg shadow-md p-4"
             >
-              View
-            </Link>
-          </motion.div>
-        ))
-      ) : (
-        <div className="flex justify-center items-center h-96">
-          <h1 className="text-3xl font-bold">
-            No data found Try Adding Jobs to{" "}
-            <Link to={"/jobs"} className="text-blue-500 underline">
-              BookMark
-            </Link>{" "}
-          </h1>
-        </div>
-      )}
+              <h3 className="text-lg font-semibold">{job.job.title}</h3>
+              <p className="text-gray-600">{job.job.company}</p>
+              <div className="flex justify-between mt-2">
+                <span className="text-sm text-gray-500">
+                  {job.job.location}
+                </span>
+                <span className="text-sm text-gray-500">{job.job.type}</span>
+              </div>
+              <p className="text-sm text-gray-500 mt-2">
+                Posted {job.job.createdAt.slice(0, 10)}
+              </p>
+              <Link
+                to={`/jobs/${job.job._id}`}
+                className="text-blue-600 hover:underline"
+              >
+                View
+              </Link>
+            </motion.div>
+          ))
+        : !isLoading && (
+            <div className="flex justify-center items-center h-96">
+              <h1 className="text-3xl font-bold">
+                No data found Try Adding Jobs to{" "}
+                <Link to={"/jobs"} className="text-blue-500 underline">
+                  BookMark
+                </Link>{" "}
+              </h1>
+            </div>
+          )}
     </div>
   );
 };
